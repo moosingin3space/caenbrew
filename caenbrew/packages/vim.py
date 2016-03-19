4f86a68554877f0cfbc1dbfcb217d41c31d1397a
Waleed Khan <me@waleedkhan.name>
2016-03-19 17:56:21 -0400

    Fix typo in Vim configuration options

--- caenbrew/packages/vim.py
+++ caenbrew/packages/vim.py
@@ -11,4 +11,4 @@ class VimPackage(ConfigurePackage):
 
     url = "ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2"
     configure_options = ["--enable-multibyte",
-                         "--enable-python-interp=yes"]
+                         "--enable-pythoninterp=yes"]
