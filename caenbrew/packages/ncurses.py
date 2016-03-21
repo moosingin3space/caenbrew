652ed9909099d23594abac9db664cd38938f3803
Waleed Khan <me@waleedkhan.name>
2016-03-20 20:51:53 -0400

    Add flags to get tmux to compile

--- caenbrew/packages/ncurses.py
+++ caenbrew/packages/ncurses.py
@@ -11,4 +11,6 @@ class NcursesPackage(ConfigurePackage):
 
     artifacts = ["include/ncurses", "lib/libncurses.a"]
     url = "ftp://invisible-island.net/ncurses/ncurses.tar.gz"
+
+    # https://groups.google.com/forum/#!topic/sage-devel/1F0b6Zpb4k0
     configure_options = ["--enable-symlinks"]
--- caenbrew/packages/tmux.py
+++ caenbrew/packages/tmux.py
@@ -15,3 +15,12 @@ class TmuxPackage(ConfigurePackage):
 
     artifacts = ["bin/tmux"]
     url = "https://github.com/tmux/tmux/releases/download/2.1/tmux-2.1.tar.gz"
+
+    def __init__(self, *args, **kwargs):
+        """Set configure options to work around tmux not finding libevent."""
+        super(TmuxPackage, self).__init__(*args, **kwargs)
+
+        # http://unix.stackexchange.com/a/17918/24718
+        prefix_dir = self._config["prefix_dir"]
+        self.configure_options = ["CFLAGS=-I{}/include".format(prefix_dir),
+                                  "LDFLAGS=-L{}/lib".format(prefix_dir)]
