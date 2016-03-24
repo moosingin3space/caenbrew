ed61c31c3aadc882decab1be6f09f1f99f4508e7
Nathan Moos <moosnat@umich.edu>
2016-03-24 16:09:30 -0400

    followed Waleed's advice wrt including a particular library

--- caenbrew/packages/wxwidgets.py
+++ caenbrew/packages/wxwidgets.py
@@ -10,7 +10,7 @@ class WxWidgetsPackage(AutotoolsPackage):
     version = "3.0.2"
 
     url = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.2/wxWidgets-3.0.2.tar.bz2"  # noqa
-    artifacts = ["bin/wx-config", "include/wx", "lib/libwx*"]
+    artifacts = ["bin/wx-config", "include/wx", "lib/libwx_baseu-3.0.so"]
 
     configure_options = [
         "--with-gtk=3"
