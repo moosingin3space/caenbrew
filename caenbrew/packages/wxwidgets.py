d8f578e07c87769fd6b07b86509558119c41cfaf
Nathan Moos <moosnat@umich.edu>
2016-03-24 16:04:22 -0400

    told flake8 to shut up about long URL

--- caenbrew/packages/wxwidgets.py
+++ caenbrew/packages/wxwidgets.py
@@ -9,7 +9,7 @@ class WxWidgetsPackage(AutotoolsPackage):
     homepage = "http://wxwidgets.org"
     version = "3.0.2"
 
-    url = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.2/wxWidgets-3.0.2.tar.bz2"
+    url = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.2/wxWidgets-3.0.2.tar.bz2"  # noqa
     artifacts = ["bin/wx-config", "include/wx", "lib/libwx*"]
 
     configure_options = [
